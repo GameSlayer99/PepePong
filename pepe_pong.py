a09fd531e68c8005c2b14a9c7087dc04e9d86036
GameSlayer99 <95058501+GameSlayer99@users.noreply.github.com>
2022-07-19 12:35:30 -0700

    Update pepe_pong.py

--- pepe_pong.py
+++ pepe_pong.py
@@ -16,22 +16,23 @@ def run_game():
     clock = pygame.time.Clock()
     pp_settings = Settings()
     screen = pygame.display.set_mode((pp_settings.screen_width, pp_settings.screen_height))
-    icon = pygame.image.load('images/Pepe.bmp')
+    icon = pygame.image.load('images/Pepe.bmp').convert_alpha()
     pygame.display.set_icon(icon)
     pygame.display.set_caption('Pepe Pong!')
     mixer.music.load('sounds/main_theme.wav')
-    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.set_volume(0.05)
     mixer.music.play(-1)
     # Establish variables.
     player = Player(pp_settings, screen)
     ball = Ball(pp_settings, screen, player)
     enemy = Enemy(pp_settings, screen, ball, player)
-    startup = Startup(screen, player, "Press SPACE to play!")
+    startup = Startup(screen, player, 'Press SPACE to play!')
 
-    # Start the main loop for the game.
+# Start the main loop for the game.
     while True:
-        gf.check_events(player, pp_settings)
+        gf.check_events(pp_settings)
         clock.tick(60)
+        print(clock.get_fps())
         if pp_settings.game_active:
             enemy.update()
             player.update()
