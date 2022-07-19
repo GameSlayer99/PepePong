1af45acf3b311f167ddae504f5e48af4c651710d
GameSlayer99 <95058501+GameSlayer99@users.noreply.github.com>
2022-07-19 12:32:46 -0700

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-from pygame import mixer
 
 
 class Ball:
@@ -12,7 +11,7 @@ class Ball:
         self.player = player
 
         # Draw the ball.
-        self.ball = pygame.image.load('images/pxArtBall.png')
+        self.ball = pygame.image.load('images/pxArtBall.png').convert_alpha()
         self.rect = self.ball.get_rect()
         self.screen_rect = screen.get_rect()
 
@@ -32,30 +31,24 @@ class Ball:
         self.rect.centery += self.pp_settings.ball_speed_factor_y
         if self.rect.top < 0 or self.rect.bottom > self.pp_settings.screen_height:
             self.pp_settings.ball_speed_factor_y *= -1
-            wall_hit = mixer.Sound('sounds/wall_hit.wav')
-            wall_hit.play()
+            self.pp_settings.wall_hit.play()
 
         # Update the score.
         if self.rect.right >= self.pp_settings.screen_width:
             self.pp_settings.enemy_score += 1
-            if self.pp_settings.game_active:
-                enemy_score = mixer.Sound('sounds/good_ending.wav')
-                enemy_score.play()
+            self.pp_settings.score.play()
             self.ball_restart()
             self.pp_settings.rally = 0
         if self.rect.left <= 0:
             self.pp_settings.player_score += 1
-            if self.pp_settings.game_active:
-                player_score = mixer.Sound('sounds/good_ending.wav')
-                player_score.play()
+            self.pp_settings.score.play()
             self.ball_restart()
             self.pp_settings.rally = 0
 
         # Reset the ball's position.
         if self.rect.colliderect(self.player.rect):
             if abs(self.rect.right - self.player.rect.left) <= 20:
-                paddle_hit = mixer.Sound('sounds/paddle_hit.wav')
-                paddle_hit.play()
+                self.pp_settings.paddle_hit.play()
                 self.pp_settings.ball_speed_factor_x = random.choice((-10, -15))
                 if self.rect.centery < 200:
                     self.pp_settings.ball_speed_factor_y = random.choice((1, 5, 10, 15))
